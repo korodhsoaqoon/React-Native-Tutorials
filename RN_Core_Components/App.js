060c853646866e9f3a65b59e9e688f2fad3c0a14
Abdullahi <hajiabdi213@gmail.com>
2025-09-16 07:37:49 +0300

    Scroll View

--- RN_Core_Components/App.js
+++ RN_Core_Components/App.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
 export default function App() {
   return (
     <ImageBackground
@@ -13,36 +13,63 @@ export default function App() {
         padding: 20,
       }}
     >
-      <Image
-        source={require("./assets/react-native.png")}
-        style={{ width: 150, height: 150 }}
-      />
-      <Text
+      <ScrollView
         style={{
-          fontSize: 28,
-          fontWeight: "bold",
-          color: "#0077b6",
+          padding: 10,
         }}
-      >
-        👋 Hello, React Native!
-      </Text>
-      <Text
-        style={{
-          fontSize: 16,
-          color: "#333",
-          textAlign: "center",
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignItems: "center",
         }}
+        showsVerticalScrollIndicator={false}
+        // horizontal
       >
-        This is your very first mobile app using Expo 🚀
-      </Text>
+        <Image
+          source={require("./assets/react-native.png")}
+          style={{ width: 150, height: 150 }}
+        />
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: "bold",
+            color: "#0077b6",
+          }}
+        >
+          👋 Hello, React Native!
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#333",
+            textAlign: "center",
+          }}
+        >
+          This is your very first mobile app using Expo 🚀
+        </Text>
 
-      <Image
-        source={{
-          uri: "https://www.evolutionithub.com/wp-content/uploads/2024/04/react-native-apps-development-1024x952.png",
-        }}
-        width={320}
-        height={320}
-      />
+        <Image
+          source={{
+            uri: "https://www.evolutionithub.com/wp-content/uploads/2024/04/react-native-apps-development-1024x952.png",
+          }}
+          width={320}
+          height={320}
+        />
+
+        <Text style={{ fontSize: 25 }}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus nulla,
+          amet aperiam non quasi magnam provident, autem, excepturi fugiat sequi
+          beatae iste sunt asperiores nesciunt doloremque recusandae nam
+          architecto obcaecati omnis eius voluptatem voluptatum vitae. Itaque
+          culpa in, pariatur fugiat reiciendis adipisci laudantium
+          necessitatibus similique provident quidem quod placeat quaerat
+          corrupti dicta nostrum quisquam eius neque ratione, qui voluptates,
+          cum totam inventore veniam rerum! Quam aliquid nemo quis ex accusamus
+          consectetur dicta repellendus eveniet temporibus recusandae laudantium
+          animi est, molestiae architecto iste ducimus exercitationem officiis
+          nesciunt doloribus cupiditate inventore possimus totam? Aperiam magni
+          illo soluta. Ex sed consequatur voluptas maxime.
+        </Text>
+      </ScrollView>
     </ImageBackground>
   );
 }
